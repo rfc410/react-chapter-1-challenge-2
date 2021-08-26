df11df36a79fb4efab56bf0bd58f42e6ae517f49
Caio Rodrigues <rfcaio90@gmail.com>
2021-08-26 00:51:07 -0300

    adiciona a solução do Desafio 02 do primeiro módulo

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from '../App';
+
+import { MovieCard } from './MovieCard';
+
+interface ContentProps {
+  movies: MovieProps[];
+  title: string;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
